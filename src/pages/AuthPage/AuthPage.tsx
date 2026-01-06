fd372acee9f031e46fc784e919b8fb2304ca9b75
rohan-hulage <hulageabhishek@gmail.com>
2026-01-06 15:04:07 +0530

    feat: Introduce global CSS variables for theming and simplify the authentication page layout.

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -5,20 +5,12 @@ const AuthPage = () => {
   return (
     <div className='auth-container'>
       <div className='auth-container-inner'>
-        <div className='auth-left'>
-          <img src='/src/assets/instagram-mobile-logo.png' alt='Instagram Mobile' />
-        </div>
+
         <div className='auth-right'>
           <AuthForm />
 
-          <div className='get-app-container'>
-            <p>Get the app.</p>
-            <div className='app-store-images'>
-              {/* Placeholders for App Store buttons, user can fill these related images later or I can try to find them if they exist in assets, for now just text or simple boxes if no image found */}
-              <img src="https://static.cdninstagram.com/rsrc.php/v3/yt/r/Yfc020c87j0.png" alt="Get it on Google Play" />
-              <img src="https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png" alt="Get it on Microsoft" />
-            </div>
-          </div>
+
+
         </div>
       </div>
     </div>
