962accb75556a90fe2f66d0f3b03221d39710ce2
rohan-hulage <hulageabhishek@gmail.com>
2026-01-07 12:01:26 +0530

    feat: add Stories component with dedicated styling and mock data, removing old story styles from homepage.

--- src/components/Feed/Stories.tsx
+++ src/components/Feed/Stories.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+
+import './stories.scss';
+import profileImg from '../../assets/profile.png';
+import userImg from '../../assets/LSH69216003-1716455496501095941-Male_iLc50OHUqdVmovPJ_60X60.webp';
+
+interface Story {
+  id: number;
+  username: string;
+  avatar: string;
+}
+
+const MOCK_STORIES: Story[] = [
+  { id: 1, username: 'johndoe', avatar: userImg },
+  { id: 2, username: 'janedoe', avatar: profileImg },
+  { id: 3, username: 'alex_smith', avatar: userImg },
+  { id: 4, username: 'sarah_j', avatar: profileImg },
+  { id: 5, username: 'mike_tyson', avatar: userImg },
+  { id: 6, username: 'emily_rose', avatar: profileImg },
+  { id: 7, username: 'chris_evans', avatar: userImg },
+  { id: 8, username: 'natasha_r', avatar: profileImg },
+  { id: 9, username: 'bruce_wayne', avatar: userImg },
+  { id: 10, username: 'clark_kent', avatar: profileImg },
+];
 
 const Stories = () => {
   return (
-      <div className='stories-container'>
-        {Array(10).fill(0).map((_, idx) => (
-          <div className='stories-circle' key={idx}>
-            <img src='src/assets/LSH69216003-1716455496501095941-Male_iLc50OHUqdVmovPJ_60X60.webp' alt='story-img' />
+    <div className='stories-wrapper'>
+      {MOCK_STORIES.map((story) => (
+        <div className='story-item' key={story.id}>
+          <div className='story-ring'>
+            <img src={story.avatar} alt={`${story.username}'s story`} className='story-avatar' />
           </div>
-        ))}
-      </div>
-  )
-}
+          <span className='story-username'>{story.username}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Stories
+export default Stories;
